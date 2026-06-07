e43b2d73f53703ed31063304ef225c6bb3e03f92
Jana-Tamer <janatamerahmed809@gmail.com>
2026-06-07 14:19:43 +0300

    dynamic navbar
    this commit is responsible for changing the navbar acoording to the authentication

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,19 +6,22 @@ import { IoCartOutline } from "react-icons/io5";
 import CartContext from "@/context/CartContext";
 import { ProductContext } from "@/context/ProductContext";
 import Link from "next/link";
+import { AuthContext } from "@/context/AuthContext";
 
 function Navbar() {
   const productsData = useContext(ProductsContext);
   const categoriesData = useContext(CategoryContext);
   const cartData = useContext(CartContext);
   const productData = useContext(ProductContext);
+  const authData = useContext(AuthContext);
 
-  if (!productsData || !categoriesData || !cartData) return null;
+  if (!productsData || !categoriesData || !cartData || !authData) return null;
 
   const { setSelectedCategory, selectedCategory } = productsData;
   const { category, error } = categoriesData;
   const { cart, addToCart } = cartData;
   const { product } = productData;
+  const { user, logout } = authData;
 
   const cartCount = cart.reduce((total, item) => {
     // reduce it loops on the array to return only one value
@@ -45,7 +48,7 @@ function Navbar() {
 
             {category?.map((cat) => (
               <button
-                className="rounded-[0.3rem] font-bold p-2 text-white"
+                className=" font-bold p-2 text-white"
                 key={cat as unknown as string}
                 onClick={() => setSelectedCategory(cat as unknown as string)}
               >
@@ -53,19 +56,30 @@ function Navbar() {
               </button>
             ))}
 
-            <Link
-              href="/login"
-              className="rounded-[0.3rem] font-bold p-2 text-white"
-            >
-              LogIn
-            </Link>
+            {!user ? (
+              <Link
+                href="/login"
+                className="rounded-[0.3rem] font-bold p-2 text-white"
+              >
+                Login
+              </Link>
+            ) : (
+              <>
+                <span className=" font-bold p-2 text-white">{user}</span>
+                <button onClick={logout}>Logout</button>
+              </>
+            )}
 
             <div className=" flex relative pt-3">
-              <IoCartOutline className="text-2xl text-white" />
-
-              <span className="absolute -top-4 -right-1 rounded-full px-2 text-sm text-white pt-3">
-                {cartCount}
-              </span>
+              <Link
+                href="/cart"
+                className="rounded-[0.3rem] font-bold p-2 text-white"
+              >
+                <IoCartOutline className="text-2xl text-white" />
+                <span className="absolute -top-4 -right-1 rounded-full px-2 text-sm text-white pt-3">
+                  {cartCount}
+                </span>
+              </Link>
             </div>
           </div>
         )}
